eca60c4bef3f36b577ed90642d3f949100490581
Neville Oronni <nevooronni@gmail.com>
2018-01-15 14:32:50 +0300

    merge with grantone

--- smarttrade/settings.py
+++ smarttrade/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'smarttrade.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smart',
+        'USER': 'khalid',
+    'PASSWORD':'pythonista50',
     }
 }
 
@@ -111,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
