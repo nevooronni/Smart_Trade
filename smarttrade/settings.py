8734a6a1d1085c543128c2690599deb044578084
Granton Mchana <gmchana254@gmail.com>
2018-01-15 15:44:13 +0300

    modified settings.py file

--- smarttrade/settings.py
+++ smarttrade/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'smart',
-        'USER': 'nevo',
-    'PASSWORD':'speeds01',
+        'USER': 'granton',
+        'PASSWORD': 'granton',
     }
 }
 
