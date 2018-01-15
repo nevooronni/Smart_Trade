45d014fa9376722249b1b0db5c26292753ac8bc1
Granton Mchana <gmchana254@gmail.com>
2018-01-15 12:35:49 +0300

    modified projects urls.py file

--- smarttrade/urls.py
+++ smarttrade/urls.py
@@ -21,4 +21,6 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include('app.urls')),
+    url(r'^accounts', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
