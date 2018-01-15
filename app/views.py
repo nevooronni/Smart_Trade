46c3658af1541fb2460e4afeb4a81e52320c6662
Neville Oronni <nevooronni@gmail.com>
2018-01-15 11:47:50 +0300

    created first template

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.conf.urls import url 
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 
 # Create your views here.
+def landing_page(request):
+	return render(request,'all-app/landing_page.html')
