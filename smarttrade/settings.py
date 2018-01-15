c8341b88e97d0617aee1e9fc05bcb126603d8fa0
Granton Mchana <gmchana254@gmail.com>
2018-01-15 12:11:35 +0300

    modifird urls.py file

--- smarttrade/settings.py
+++ smarttrade/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.apps.AppConfig',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
@@ -62,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
