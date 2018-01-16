9752f2dfb83704fbf21da8d80fd18a6d7d82cdeb
khalid hassan <abuuanas30@hotmail.com>
2018-01-16 14:10:30 +0300

    created index view

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render,redirect
-from django.conf.urls import url 
+from django.conf.urls import url
 from django.http import HttpResponse,Http404,HttpResponseRedirect
+from .models import Product,Profile
 
 # Create your views here.
 def landing_page(request):
 	return render(request,'all-app/landing_page.html')
+
+def index(request):    
+	all_products = Product.get_all_products()
+	return render(request, 'index.html',{"products": all_products})
