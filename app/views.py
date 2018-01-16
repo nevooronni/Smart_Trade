da4aaa390fce7c8f8995fd3c707b82886da5033a
Granton Mchana <gmchana254@gmail.com>
2018-01-16 11:30:22 +0300

    Created new files email.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render,redirect
-from django.conf.urls import url 
-from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.conf.urls import url
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
+
+
 def landing_page(request):
-	return render(request,'all-app/landing_page.html')
+    return render(request, 'all-app/landing_page.html')
+
+
+def post(request, pk):
+    post = Post.objects.get(pk=post_id)
+
+    return render(request, 'all-app/post.html')
