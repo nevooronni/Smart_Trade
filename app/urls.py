da6bdd0153d9625c638a2a3eb368ccf4bbc7fdcb
khalid hassan <abuuanas30@hotmail.com>
2018-01-16 14:33:11 +0300

    added a class_based generic view

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns=[
 	url('^$',views.landing_page,name="landing_page"),
 	url(r'', views.index, name="index")
-		
+	path('product/', views.ProductDetailtView.as_view(), name='product'),
+
+
 ]
--- app/views.py
+++ app/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render,redirect
 from django.conf.urls import url
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .models import Product,Profile
+from django.views import generic
 
 # Create your views here.
 def landing_page(request):
 	return render(request,'all-app/landing_page.html')
 
-def index(request):    
+def index(request):
 	all_products = Product.get_all_products()
 	return render(request, 'index.html',{"products": all_products})
+
+
+
+class ProductDetailView(generic.DetailView):
+    model = Product
