231f0a24c1da70a24effed7b6d81762e706c8e14
Neville Oronni <nevooronni@gmail.com>
2018-01-18 01:25:41 +0300

    added login home template

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url 
 from django.conf import settings
+from django.conf.urls.static import static
 from . import views 
 
 urlpatterns=[
 	url('^$',views.landing_page,name="landing_page"),
 	url('^index/$',views.index,name = "index"),	
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
