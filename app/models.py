284b9d89c076749743cde9198d630ec901256ff8
khalid hassan <abuuanas30@hotmail.com>
2018-01-18 16:52:21 +0300

    added a cart

--- app/models.py
+++ app/models.py
@@ -5,15 +5,21 @@ from django.db.models.signals import post_save
 from django.db.models import Q, signals
 from django.dispatch import receiver
 from phonenumber_field.modelfields import PhoneNumberField
-# from email_field.modelfields import EmailField
+from datetime import datetime
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.contenttypes.models import ContentType
+
+
 
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=140, blank=True)
+    last_name = models.CharField(max_length=140, blank=True)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
     phone_number = PhoneNumberField()
-    locatiom = models.CharField(max_length=140, blank=True)
+    location = models.CharField(max_length=140, blank=True)
     email = models.EmailField(max_length=140, blank=True)
 
 
@@ -34,41 +40,56 @@ def save_user_profile(sender, instance, **kwargs):
 
 post_save.connect(create_user_profile, sender=User)
 
-class Product(models.Model):
-    name = models.CharField(max_length= 140, blank = True)
-    unit_quantity = models.FloatField(default= 0.0)
-    price = models.FloatField(default = 0.0)
-    category = models.CharField(max_length = 140, blank =True)
-    description = models.TextField(max_length=140)
+class Item(models.Model):
+    cart = models.ForeignKey('Cart', on_delete = models.CASCADE, related_name = 'cart')
+    quantity = models.PositiveIntegerField(verbose_name=_('quantity'))
+    unit_price = models.DecimalField(max_digits=18, decimal_places=2, verbose_name=_('unit price'))
+    content_type = models.ForeignKey(ContentType, on_delete = models.CASCADE, related_name= 'content_type')
+    object_id = models.PositiveIntegerField()
 
-
+    #objects = ItemManager()
 
     class Meta:
-        ordering = ['category']
+        verbose_name = _('item')
+        verbose_name_plural = _('items')
+        ordering = ('cart',)
 
-    @classmethod
-    def get_all_products(cls):
-        products = cls.objects.all()
-        return products
+    def __unicode__(self):
+        return u'%d units of %s' % (self.quantity, self.product.__class__.__name__)
 
-    @classmethod
-    def get_single_product(cls, pk):
-        product = cls.objects.get(pk=pk)
-        return product
+    def total_price(self):
+        return self.quantity * self.unit_price
+        total_price = property(total_price)
 
-    @classmethod
-    def display_users_products(cls, id):
-        products = cls.objects.filter(user_id=id)
-        return products
+    # product
+    def get_product(self):
+        return self.content_type.get_object_for_this_type(pk=self.object_id)
 
-    def get_absolute_url(self):
-        """
-        Returns the url to access a particular product instance.
-        """
-        return reverse('product-detail', args=[str(self.id)])
+    def set_product(self, product):
+        self.content_type = ContentType.objects.get_for_model(type(product))
+        self.object_id = product.pk
 
-    def __str__(self):
-        """
-        String for representing the Model object.
-        """
-        return self.name
+    product = property(get_product, set_product)
+
+
+
+
+class Cart(models.Model):
+    creation_date = models.DateTimeField(verbose_name=_('creation date'))
+    checked_out = models.BooleanField(default=False, verbose_name=_('checked out'))
+
+    class Meta:
+        verbose_name = _('cart')
+        verbose_name_plural = _('carts')
+        ordering = ('-creation_date',)
+
+    def __unicode__(self):
+        return unicode(self.creation_date)
+
+class ItemManager(models.Manager):
+    def get(self, *args, **kwargs):
+        if 'product' in kwargs:
+            kwargs['content_type'] = ContentType.objects.get_for_model(type(kwargs['product']))
+            kwargs['object_id'] = kwargs['product'].pk
+            del(kwargs['product'])
+        return super(ItemManager, self).get(*args, **kwargs)
--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import url
+from django.urls import path,re_path
 from django.conf import settings
 from . import views
 
 urlpatterns=[
-	url('^$',views.landing_page,name="landing_page"),
-	url(r'', views.index, name="index")
-	path('product/', views.ProductDetailtView.as_view(), name='product'),
+	re_path('^$',views.landing_page,name="landing_page"),
+	re_path(r'', views.index, name="index"),
+
+
 
 
 ]
