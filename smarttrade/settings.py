295fe0d37be2d848b568bfb1713a2fce2a868279
Neville Oronni <nevooronni@gmail.com>
2018-01-22 16:52:36 +0300

    added db driver

--- smarttrade/settings.py
+++ smarttrade/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'smarttrade.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'smart',
         'USER': 'nevo',
     'PASSWORD':'speeds01',
