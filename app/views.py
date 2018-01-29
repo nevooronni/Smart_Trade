896835cabb37fa6a3ea005b20c4f8aef1dc618c9
Granton Mchana <gmchana254@gmail.com>
2018-01-29 16:10:28 +0300

    modified views.py file

--- app/views.py
+++ app/views.py
@@ -259,3 +259,25 @@ def profile(request, user_id):
     user = User.objects.get(pk=user_id)
     user.profile.username = 'first_name, last_name'
     user.save()
+
+
+@login_required
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, _(
+                'Your profile was successfully updated!'))
+            return redirect('settings:profile')
+        else:
+            messages.error(request, _('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'profiles/profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
